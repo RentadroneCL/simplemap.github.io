bde6508c3daf2506f8bed2ff21507d40e703f637
Hector Bastidas <hector.bastidas@simplemap.io>
2023-01-04 18:11:05 -0300

    Updated base URL

---
+++ build/assets/js/b645a03e.c00815ca.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[547],{2596:e=>{e.exports=JSON.parse('{"permalink":"/simplemap.github.io/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":5,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
